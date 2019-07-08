03469906e10f9230b799fa20ff8eddf441fb6c04
Git-noob1 <52640721+Git-noob1@users.noreply.github.com>
2019-07-08 10:11:52 +0800

    Update Helloworld.java

--- src/helloworld/Helloworld.java
+++ src/helloworld/Helloworld.java
@@ -3,6 +3,6 @@ package helloworld;
 public class Helloworld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World 我再改一哈哈");
+		System.out.println("Hello World 你改个吉尔");
 	}
 }
